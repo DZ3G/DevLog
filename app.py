98bc1acda4b6842ace81fb82051c21b88cb1b639
momo <berrachedrafik11@gmail.com>
2026-06-04 12:31:29 +0200

    added a 404 error : page not found

--- app.py
+++ app.py
@@ -106,6 +106,10 @@ def delete_post(post_id):
         db.session.commit()
     return redirect(url_for('index'))
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 
 if __name__ == '__main__':
     with app.app_context():
